4aa4415731718df2d0ff1739a527e6e77940caec
Houssem Amiri <houssem.amiri2@gmail.com>
2022-08-12 09:46:50 +0100

    add testing of app.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+describe("App component", () => {
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test("Edit src/App.tsx and save to reload", () => {
+    render(<App />);
+    const textContent = screen.getByText(/edit and save to reload\./i);
+    expect(textContent).toBeInTheDocument();
+  });
+
+  test("if the banner exist", () => {
+    render(<App />);
+    const banner = screen.getByRole("banner");
+    expect(banner).toBeInTheDocument();
+  });
+
+  test("codesweetly test heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading")).toHaveTextContent(/codesweetly test/i);
+  });
+
+  test("a codesweetly project heading", () => {
+    render(<App />);
+
+    const button = screen.getByRole("button", { name: "Update Heading" });
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading")).toHaveTextContent(
+      /a codesweetly project/i
+    );
+  });
+
+  
+  test('verify if link exist', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId('learn-link');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('verify text in the link', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId('learn-link');
+    expect(linkElement).toHaveTextContent(
+      /Visit Houssem website/i
+    );
+  });
+
+
+  test('url is correct', ()=>{
+    render(<App />)
+    const linkElement = screen.getByTestId('learn-link').closest('a');
+    expect(linkElement?.href).toContain('https://houssemamiri.com')
+  })  
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [heading, setHeading] = useState("CodeSweetly Test");
+
+  const handleClick = () => {
+    setHeading("A CodeSweetly Project");
+  };
   return (
     <div className="App">
       <header className="App-header">
+      <h1>{heading}</h1>
+        <button type="button" onClick={handleClick}>
+          Update Heading
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://houssemamiri.com"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid="learn-link"
         >
-          Learn React
+          Visit Houssem website
         </a>
       </header>
     </div>
